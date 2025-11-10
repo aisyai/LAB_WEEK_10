796eeecd9fb1249e55e099b3c727da79e4bcd9d2
aisyai <aisyaadiyann@gmail.com>
2025-11-10 16:07:43 +0700

    Commit No. 1: Implement ViewModel to Handle Rotation

--- app/src/main/java/com/example/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -5,16 +5,60 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.example.lab_week_10.viewmodels.TotalViewModel
+import kotlin.getValue
+import kotlin.lazy
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel by lazy {
+        ViewModelProvider(this)[TotalViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        prepareViewModel()
+    }
+
+    private fun updateText(total: Int) {
+        findViewById<TextView>(R.id.text_total).text =
+            getString(R.string.text_total, total)
+    }
+
+    private fun prepareViewModel(){
+        viewModel.total.observe(this) { total ->
+            updateText(total)
+        }
+
+        findViewById<Button>(R.id.button_increment).setOnClickListener {
+            viewModel.incrementTotal()
         }
     }
+
+    /*private var total: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        total = 0
+        updateText(total)
+        findViewById<Button>(R.id.button_increment).setOnClickListener {
+            incrementTotal()
+        }
+    }
+    private fun incrementTotal() {
+        total++
+        updateText(total)
+    }
+    private fun updateText(total: Int) {
+        findViewById<TextView>(R.id.text_total).text =
+            getString(R.string.text_total, total)
+    }*/
 }
